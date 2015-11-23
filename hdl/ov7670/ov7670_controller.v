7a05d1b5ee20e7755b381bbad7298327f772876d
Mike Wild <mikeanthonywild@gmail.com>
2015-11-23 15:17:58 +0000

    Add OV7670 controller module

---
+++ hdl/ov7670/ov7670_controller.v
@@ -0,0 +1,17 @@
+/* 
+ * ov7670_controller.v
+ *
+ * Author: Mike Wild <m.a.wild@se12.qmul.ac.uk>
+ * Date: 23rd November 2015
+ *
+ * This module connects the OV7670 register initialisation routines and I2C
+ * controller together and trigger them on startup. Once initialisation is
+ * finished, it sets a done flag to indicate that capture may proceed.
+ */
+
+ module ov7670_controller (
+
+ );
+
+
+ endmodule

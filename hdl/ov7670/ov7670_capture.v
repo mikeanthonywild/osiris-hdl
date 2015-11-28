e03f262f1665b2f130b0014f7cc071ef0321d0e2
Mike Wild <mikeanthonywild@gmail.com>
2015-11-28 15:19:30 +0000

    Convert ov7670_capture module to QVGA

--- hdl/ov7670/ov7670_capture.v
+++ hdl/ov7670/ov7670_capture.v
@@ -5,26 +5,26 @@
  * Date: 14th October 2015
  *
  * Module to capture pixel data from OV7670 image sensor. OV7670 requires a
- * clock input to drive 24 MHz pixel clock required for 30 FPS. This module
+ * clock input to drive 12 MHz pixel clock required for 30 FPS. This module
  * starts acquiring pixel data after OV7670 registers have been configured.
- * Pixel data is stored in a framebuffer – QCIF image resolution (176x132) is
+ * Pixel data is stored in a framebuffer – QVGA image resolution (320x240) is
  * used due to device block RAM constraints on the Spartan-3E FPGA.
  */
 
 module ov7670_capture (
-    input               pclk_24,    // 24MHz Pixel clock
+    input               pclk_12,    // 12MHz Pixel clock
     input               reset_n,    // Synchronous reset
     input               start,      // Start capturing
     input               vsync,      //Vertical sync signal
     input               href,       // Horizontal timing reference
     input [7:0]         d,          // RAW pixel data from sensor
-    output reg [15:0]   addr,       // Framebuffer address
-    output reg [7:0]    dout        // Data to write to framebuffer
+    output reg [16:0]   addr,       // Framebuffer address
+    output reg [2:0]    dout        // Data to write to framebuffer
 );
 
-    reg [15:0] next_addr;
+    reg [16:0] next_addr;
 
-    always @(posedge pclk_24) begin
+    always @(posedge pclk_12) begin
         if (!reset_n) begin
             // Reset everything
             addr <= 0;
@@ -36,7 +36,7 @@ module ov7670_capture (
                     addr <= 0;
                 end else if (href) begin
                     // Clock data from sensor into framebuffer
-                    dout <= d;
+                    dout <= d[7:5];
                     addr <= next_addr;  // Output address must lag by 1
                     next_addr <= next_addr + 1;
                 end

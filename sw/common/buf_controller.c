ded5aa24be6aef19bdc981e861ad6125380f3dd8
Mike Wild <mikeanthonywild@gmail.com>
2016-03-24 12:04:56 +0000

    Add barebones firmware files

---
+++ sw/common/buf_controller.c
@@ -0,0 +1,30 @@
+/*
+ * buf_controller.c
+ *
+ * Author: Mike Wild <m.a.wild@se12.qmul.ac.uk>
+ * Date: 24th March 2016
+ *
+ * Handles buf_controller interrupts from fabric and manages
+ * linebuffer-framebuffer transfers.
+ *
+ */
+
+/***************************** Include Files *********************************/
+#include "framebuf.h"
+
+/************************** Constant Definitions *****************************/
+
+
+/**************************** Type Definitions *******************************/
+
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+
+/************************** Variable Definitions *****************************/
+
+
+/************************** Function Prototypes ******************************/
+
+
+/*****************************************************************************/
---
+++ sw/common/framebuf.c
@@ -0,0 +1,30 @@
+/*
+ * framebuf.c
+ *
+ * Author: Mike Wild <m.a.wild@se12.qmul.ac.uk>
+ * Date: 24th March 2016
+ *
+ * Library for accessing the global framebuffer 
+ *
+ */
+
+/***************************** Include Files *********************************/
+#include "framebuf.h"
+#include "config.h"
+
+/************************** Constant Definitions *****************************/
+
+
+/**************************** Type Definitions *******************************/
+
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+
+/************************** Variable Definitions *****************************/
+u8 framebuf[FRAMEBUF_HEIGHT][FRAMEBUF_WIDTH]
+
+/************************** Function Prototypes ******************************/
+
+
+/*****************************************************************************/
